16a02ce9f03533911316f84aa82f88ded80627de
Paul Spende <paul.spende@nexenio.com>
2020-11-10 15:14:47 +0100

    fix heroku deploy

--- server.js
+++ server.js
@@ -208,7 +208,7 @@ fastify.route({
 
 const start = async () => {
   try {
-    await fastify.listen(process.env.PORT || 3000);
+    await fastify.listen(process.env.PORT || 3000, "0.0.0.0");
     fastify.log.info(`server listening on ${fastify.server.address().port}`);
   } catch (err) {
     fastify.log.error(err);
