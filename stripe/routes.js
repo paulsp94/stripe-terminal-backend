a5be841fadb6d417f7bb8aa8490bb905c5243bb6
Paul Spende <paul.spende@nexenio.com>
2021-03-02 23:59:19 +0100

    fix imports

--- stripe/routes.js
+++ stripe/routes.js
@@ -1,42 +1,42 @@
-const {schema, controller} = require('.');
+const {stripeSchema, stripeController} = require('.');
 
 module.exports = (fastify, opts, done) => {
-  fastify.addHook("preHandler", controller.preHandler);
+  fastify.addHook("preHandler", stripeController.preHandler);
 
   // This endpoint registers a WisePay 3 reader to your Stripe account.
   fastify.post(
     "/register_readers",
-    schema.registerReaderSchema,
-    controller.registerReaderController
+    stripeSchema.registerReaderSchema,
+    stripeController.registerReaderController
   );
 
   //  This endpoint creates a ConnectionToken, which gives the SDK permission
   //  to use a reader with your Stripe account.
   fastify.post(
     "/connection_token",
-    schema.connectionTokenSchema,
-    controller.connectionTokenController
+    stripeSchema.connectionTokenSchema,
+    stripeController.connectionTokenController
   );
 
   // This endpoint creates a PaymentIntent.
   fastify.post(
     "/create_payment_intent",
-    schema.createPaymentIntentSchema,
-    controller.createPaymentIntentController
+    stripeSchema.createPaymentIntentSchema,
+    stripeController.createPaymentIntentController
   );
 
   // This endpoint captures a PaymentIntent.
   fastify.post(
     "/capture_payment_intent",
-    schema.capturePaymentIntentSchema,
-    controller.capturePaymentIntentController
+    stripeSchema.capturePaymentIntentSchema,
+    stripeController.capturePaymentIntentController
   );
 
   // This endpoint attaches a PaymentMethod to a Customer.
   fastify.post(
     "/attach_payment_method_to_customer",
-    schema.attachPaymentMethodToCustomerSchema,
-    controller.attachPaymentMethodToCustomerController
+    stripeSchema.attachPaymentMethodToCustomerSchema,
+    stripeController.attachPaymentMethodToCustomerController
   );
 
   done();
